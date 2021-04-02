fb55d7051adba907f10c24f82456993717a3c7f6
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-02 19:01:44 -0400

    Include bodyParser and Mongo

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 app.listen(3000, function () {
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  MongoClient.connect("mongodb-connection-string", (err, client) => {
+    // ... do something here
+  });
+
   app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   });
-});
 
+  app.post("/posts", (req, res) => {
+    console.log("Hellooooooooooooooooo!");
+  });
+});
