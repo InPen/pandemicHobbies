fc8f4b860aa9d9c0f81c6021a2afbe20342edab7
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-03 11:29:50 -0400

    requiere dotenv file and create db connection

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const { MongoServerSelectionError } = require("mongodb");
 const app = express();
 
-app.listen(3000, function () {
-  app.use(bodyParser.urlencoded({ extended: true }));
 
-  MongoClient.connect("mongodb-connection-string", (err, client) => {
-    // ... do something here
-  });
+//environment variables
+require(‘dotenv’).config();
 
-  app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  });
 
-  app.post("/posts", (req, res) => {
-    console.log("Hellooooooooooooooooo!");
-  });
-});
+//database connection
+
+const uri = process.env.MONGO_URI;
+MongoServerSelectionError.connect(uri, {useNewUrlParser:true,useCreateIndex:true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Connected to DB sucessfully!')
+})
+
+
+// app.listen(3000, function () {
+//   app.use(bodyParser.urlencoded({ extended: true }));
+
+//   MongoClient.connect("mongodb-connection-string", (err, client) => {
+//     // ... do something here
+//   });
+
+//   app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+//     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+//     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+//   });
+
+//   app.post("/posts", (req, res) => {
+//     console.log("Hellooooooooooooooooo!");
+//   });
+// });
