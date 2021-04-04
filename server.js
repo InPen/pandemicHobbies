219efad72eb7d7268592447aabfe458dd18cbac8
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-04 16:06:23 -0400

    declared db globally. Refactor post route

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const { MongoServerSelectionError } = require("mongodb");
 const app = express();
 //environment variables
 const {MONGO_URI} = require("./.env");
+var db
 
-
-//database connection
 
 
 
@@ -17,17 +16,24 @@ app.listen(3000, function () {
 
 MongoClient.connect(MONGO_URI, { useUnifiedTopology: true }).then((client) => {
   console.log("Connected to Database");
-  const db = client.db("pandemicHobbies");
+  db = client.db("pandemicHobbies");
+  const postsCollection = db.collection("posts");
+
 });
 
 
   app.get("/", (req, res) => {
+    const cursor = db.collection("posts").find();
+    console.log(cursor);
     res.sendFile(__dirname + "/index.html");
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   });
 
   app.post("/posts", (req, res) => {
-    console.log("Hellooooooooooooooooo!");
+    db.collection('posts').save(req.body, (err, result) => {
+      if (err){ return console.log(err)}
+      console.log('saved to database')
+      res.redirect('/')  })
   });
 });
