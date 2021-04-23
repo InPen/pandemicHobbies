2b39761a9712ac25612a780b92bc0724ff01620f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-04-23 14:28:25 -0400

    refactored routes, currently working on Update

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,14 @@
 module.exports = function (app, passport, db) {
-  
-  app.get("/", (req, res) => {
-      console.log(db.collection("posts"))
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+  app.get("/", function (req, res) {
+    //console.log(req, res);
     db.collection("posts")
       .find()
       .toArray()
       .then((results) => {
-        res.render("index.ejs", { posts: results });
-      });
-  });
- 
-  app.get("/profile", isLoggedIn, (req, res) => {
-    db.collection("posts")
-      .find()
-      .toArray()
-      .then((results) => {
-        res.render("profile.ejs", { posts: results });
+        res.render("index.ejs", { posts: results, user: req.user });
       });
   });
 
@@ -29,10 +22,59 @@ module.exports = function (app, passport, db) {
     });
   });
 
+  app.put("/posts", (req, res) => {
+    db.collection("posts").findOneAndUpdate(
+      { name: req.body.name, msg: req.body.msg },
+      // {
+      //   $set: {
+      //     thumbUp: req.body.thumbUp + 1,
+      //   },
+      // },
+      {
+        sort: { _id: -1 },
+        upsert: true,
+      },
+      (err, result) => {
+        if (err) return res.send(err);
+        res.send(result);
+      }
+    );
+  });
+
+  // PROFILE SECTION =========================
+  app.get("/profile", isLoggedIn, function (req, res) {
+    db.collection("posts")
+      .find()
+      .toArray((err, result) => {
+        if (err) return console.log(err);
+        res.render("profile.ejs", {
+          user: req.user,
+          posts: result,
+        });
+      });
+  });
+
   // LOGOUT ==============================
   app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
+  });
+
+  app.delete("/posts", (req, res) => {
+    console.log(req.body);
+    console.log("sdf");
+    debugger;
+    db.collection("posts").findOneAndDelete(
+      {
+        title: req.body.title,
+      },
+      (err, result) => {
+        console.log(err, result);
+        debugger;
+        if (err) return res.send(500, err);
+        res.send("Message deleted!");
+      }
+    );
   });
 
   // =============================================================================
@@ -43,7 +85,7 @@ module.exports = function (app, passport, db) {
   // LOGIN ===============================
   // show the login form
   app.get("/login", function (req, res) {
-    res.render("login.ejs");
+    res.render("login.ejs", { message: req.flash("loginMessage") });
   });
 
   // process the login form
@@ -59,7 +101,7 @@ module.exports = function (app, passport, db) {
   // SIGNUP =================================
   // show the signup form
   app.get("/signup", function (req, res) {
-    res.render("signup.ejs");
+    res.render("signup.ejs", { message: req.flash("signupMessage") });
   });
 
   // process the signup form
@@ -71,11 +113,33 @@ module.exports = function (app, passport, db) {
       failureFlash: true, // allow flash messages
     })
   );
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get("/unlink/local", isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect("/profile");
+    });
+  });
 };
 
-//If not logged in redirect to "/"
+function userDetails(req, res, next) {
+  req.isAuthenticated();
+  return next();
+}
 
+// route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
+
   res.redirect("/");
 }
